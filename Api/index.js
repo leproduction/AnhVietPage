bfb183d6d552092ca2a5f5ed6e5ff1f9282e49c7
leproduction <108595211+leproduction@users.noreply.github.com>
2024-05-29 13:39:24 -0400

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -1,47 +1,40 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const RegisterModel = require('./Register.js');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const RegisterModel = require('./Register.js')
 
-const app = express();
-
+const app = express()
 app.use(cors({
     origin: ["https://anh-viet-page.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
-
-app.use(express.json());
-
+app.use(express.json())
 mongoose.connect('mongodb+srv://portfolio:port@portfolio.rsdq3hc.mongodb.net/?retryWrites=true&w=majority')
-.then(() => {
-    console.log("Connected to MongoDB successfully");
-})
-.catch((error) => {
-    console.error("Error connecting to MongoDB", error);
-});
-
+    .then(() => {
+        console.log("Connected to MongoDB successfully");
+    })
+    .catch((error) => {
+        console.error("Network Error", error);
+    });
 app.get("/", (req, res) => {
     res.json("Information");
-});
-
-app.post('/submit', async (req, res) => {
-    try {
-        const { name, email, tel, note } = req.body;
-        const user = await RegisterModel.findOne({ email });
-
-        if (user) {
-            res.json("Already submitted");
+})
+app.post('/submit', (req, res) => {
+    const {name, email, tel, note} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            res.json("Already submitted")
         } else {
-            const newUser = await RegisterModel.create({ name, email, tel, note });
-            res.json(newUser);
+            RegisterModel.create({name: name, email: email, tel: tel, note: note})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
         }
-    } catch (error) {
-        console.error("Error during submission:", error);
-        res.status(500).json({ message: "Submission error", error });
-    }
-});
+    }).catch(err => res.json(err))
+})
+
 
 app.listen(3001, () => {
-    console.log("Server is running on port 3001");
-});
+    console.log("Server is Running")
+})
