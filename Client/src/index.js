0841bff6352c37614fd4a876f27c4a37e759b1ff
leproduction <108595211+leproduction@users.noreply.github.com>
2024-07-25 14:17:51 -0400

    Update index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,25 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
+
 
 import { MyProvider } from './Component/MyContext';
-=======
-import { BrowserRouter } from 'react-router-dom';
 
->>>>>>> origin/main
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-<<<<<<< HEAD
+
     <MyProvider>
       <App />
       </MyProvider>
-=======
-      <App />
 
->>>>>>> origin/main
   </React.StrictMode>
 
 );
