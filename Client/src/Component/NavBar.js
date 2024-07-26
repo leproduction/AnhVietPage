b9f208e19f92e6de74d741c3dd65993603446294
leproduction <108595211+leproduction@users.noreply.github.com>
2024-07-26 14:59:14 -0400

    Update NavBar.js

--- Client/src/Component/NavBar.js
+++ Client/src/Component/NavBar.js
@@ -20,7 +20,7 @@ const logOutButton=()=>{
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://anh-viet-page-gx18.vercel.app/signin", {
+      const response = await axios.post("https://anh-viet-page-data.vercel.app/signin", {
         email,
         password,
 
