d894769ac762714a6e6de607af35221fc4007011
leproduction <108595211+leproduction@users.noreply.github.com>
2024-07-30 13:14:45 -0400

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "https://anhviet.vercel.app//",
+    origin: "https://anhviet.vercel.app/",
     methods: ["POST", "GET", "OPTIONS"],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
