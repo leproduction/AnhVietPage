3d7e3efcb32aff3ba38899532e5923d9c1367492
leproduction <108595211+leproduction@users.noreply.github.com>
2024-09-24 15:34:23 -0400

    Update Contact.js

--- Client/src/Component/Contact.js
+++ Client/src/Component/Contact.js
@@ -14,7 +14,7 @@ export default function ContactEmer() {
     <Container  id="footer" style={{ background: "rgba(189,195,199, 0.3)" }}  align="center" fluid className='shadow rounded mx-0 my-1 py-3'>
         { contact.map(mycontact => (<Row key={mycontact.phone}>
 
-            <Col ><a className='text-decoration-none' href="tel:954-842-9190"><p className='text-black'>{mycontact.phone}</p></a>
+            <Col ><a className='text-decoration-none' href="tel:7544446941"><p className='text-black'>{mycontact.phone}</p></a>
             </Col>
 
             <Col><a className='text-decoration-none' href="mailto:goigiupvietusa@gmail.com"><p className='text-dark'>{mycontact.email}</p></a>
