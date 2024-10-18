32a41a9350ef9153c0c115cc7b6da7975d183734
leproduction <108595211+leproduction@users.noreply.github.com>
2024-10-18 10:53:45 -0400

    Update Home.js

--- Client/src/Home.js
+++ Client/src/Home.js
@@ -30,7 +30,7 @@ function Home() {
 <li>Thuê nhà</li>
 <li>Du lịch</li>
 <li>Hoàn tiền, Trả lại, Khiếu nại</li>
-<li>Tạo trang Websites, ứng dụng APPS, dịch vụ ATM, nhà cung cấp thẻ tín dụng</li>
+<li>Tạo trang Websites, ứng dụng Apps, dịch vụ ATM, nhà cung cấp thẻ tín dụng</li>
   <li>và các dịch vụ khác</li></ol>
       </Row>
  <ContactEmer />
