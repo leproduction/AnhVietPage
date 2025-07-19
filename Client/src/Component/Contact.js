7808692655da4a8ae37372a03acea9f053dad506
leproduction <108595211+leproduction@users.noreply.github.com>
2025-07-19 08:18:50 -0400

    Update Contact.js

--- Client/src/Component/Contact.js
+++ Client/src/Component/Contact.js
@@ -4,7 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import {Container, Row, Col } from 'react-bootstrap';
 const contact = [ {
 
-    phone: "754-444-6941",
+    phone: "954-637-3839",
     email: "rmstraps@gmail.com"
 
 }]
@@ -14,7 +14,7 @@ export default function ContactEmer() {
     <Container  id="footer" style={{ background: "rgba(189,195,199, 0.3)" }}  align="center" fluid className='shadow rounded mx-0 my-1 py-3'>
         { contact.map(mycontact => (<Row key={mycontact.phone}>
 
-            <Col ><a className='text-decoration-none' href="tel:754-444-6941"><p className='text-black'>{mycontact.phone}</p></a>
+            <Col ><a className='text-decoration-none' href="tel:954-637-3839"><p className='text-black'>{mycontact.phone}</p></a>
             </Col>
 
             <Col><a className='text-decoration-none' href="rmstraps2025@gmail.com"><p className='text-dark'>{mycontact.email}</p></a>
